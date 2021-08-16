d6cb9a05a3ed21a572a33c0fad87917a1bf82c4d
Stephen Garner <stephengarner@Stephens-MBP.hitronhub.home>
2021-08-15 21:05:12 -0600

    updated styling

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ GPIO.setmode(GPIO.BCM)      # We are using the BCM pin numbering
 # Declaring Servo Pins as output pins
 GPIO.setup(2, GPIO.OUT)
 
-
 # Flask constructor takes the name of current module (__name__) as argument.
 app = Flask(__name__)
 # Enable debug mode
@@ -18,8 +17,9 @@ TPL = '''
 <html>
     <head><title>Web Application to control air conditioner water relief pump</title></head>
     <body>
-    <h2> Web Application to Control Pump</h2>
-        <a href="/test">Turn pump on</a>
+    <h1 style='text-align:center;font-family:arial;'> Web Application to Control Pump</h1>
+        <div style='text-align:center;'>
+        <a href="/run" style='font-size:20px; font-family: arial;'>Turn pump on</a></div>
     </body>
 </html>
 '''
@@ -29,12 +29,13 @@ TPL = '''
 def home():
     return render_template_string(TPL)
 
-@app.route("/test")
-def test():
+@app.route("/run")
+def run():
     returnPage = ''' 
 <html>
-<h1>motor is running</h1>
-<a href="/turnoff">turn pump off</a>
+<h1 style='text-align:center;font-family:arial;'>motor is running</h1>
+<div style='text-align:center;'>
+<a href="/turnoff" style='font-size:20px; font-family: arial;'>turn pump off</a></div>
 </html>
 '''
     # Get slider Values
@@ -47,13 +48,13 @@ def turnoff():
     GPIO.output(2, GPIO.LOW)
     returnPage1 = '''
 <html>
-<h1>motor is off</h1>
-<a href="/">return to turn on</a>
+<h1 style='text-align:center;font-family: arial;'>motor is off</h1>
+<div style='text-align:center;'>
+<a href="/" style='font-size:20px; font-family: arial;'>return to turn on</a></div>
 </html>
 '''
 
     return returnPage1
-
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
